8b2b10ccdb5f38896686e61fee7d65fa1620e3bc
SHEFFmachine <sheff@sheffmachine.com>
2018-08-14 21:00:26 -0700

    We now have a working ssr app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const app = express()
 const React = require('react')
 const renderToString = require('react-dom/server').renderToString
-const Home = require('../client/components/Home')
+const Home = require('../client/components/Home').default
+const app = express()
 
 app.get('/', (req, res) => {
   const content = renderToString(<Home />)
   res.send(content)
 })
 
-app.listen(3000, () => 'Listening on port 3000...')
+app.listen(3000, () => console.log('Listening on port 3000...'))
