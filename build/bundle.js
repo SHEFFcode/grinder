df59ea30c26bdd6ba522b24eed335751eb38d73a
SHEFFmachine <sheff@sheffmachine.com>
2018-08-14 21:29:27 -0700

    React app is ssr and hydrated

--- build/bundle.js
+++ build/bundle.js
@@ -7019,9 +7019,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
-  res.send(content);
+  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
+  res.send(html);
 });
 
 app.listen(3000, function () {
@@ -22146,7 +22148,9 @@ var Home = function Home() {
     ),
     _react2.default.createElement(
       'button',
-      { onClick: console.log('Hi there!') },
+      { onClick: function onClick() {
+          return console.log('Hi there!');
+        } },
       'Press me'
     )
   );
