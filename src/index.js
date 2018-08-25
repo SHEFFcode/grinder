a0571ada39cffa4e6b45a1624f6b1eea1590e064
SHEFFmachine <sheff@sheffmachine.com>
2018-08-24 21:27:30 -0700

    Initial set up for both the client side and server side redux stores

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import renderer from '../src/helpers/renderer'
 const app = express()
+import createStore from './helpers/createStore'
 
 app.use(express.static('public'))
 app.get('*', (req, res) => {
-  res.send(renderer(req))
+  const store = createStore()
+  //init and load data into the store
+  res.send(renderer(req, store))
 })
 
 app.listen(3000, () => console.log('Listening on port 3000...'))
