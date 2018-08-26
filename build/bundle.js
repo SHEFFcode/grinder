f19deeb9e1f24efe4348ca8a8ec35f3a1f04ea62
Jeremy Shefer <yuriy.shefer@creditkarma.com>
2018-08-26 15:48:33 -0700

    Protect our app from xss

--- build/bundle.js
+++ build/bundle.js
@@ -201,6 +201,10 @@ var _Routes2 = _interopRequireDefault(_Routes);
 
 var _reactRouterConfig = __webpack_require__(2);
 
+var _serializeJavascript = __webpack_require__(19);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _reactRedux = __webpack_require__(10);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
@@ -220,7 +224,7 @@ exports.default = function (req, store) {
     )
   ));
 
-  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script>\n        window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
+  var html = '\n  <html>\n    <head></head>\n    <body>\n      <div id="root">' + content + '</div>\n      <script>\n        window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '\n      </script>\n      <script src="bundle.js"></script>\n    </body>\n  </html>\n  ';
   return html;
 };
 
@@ -510,6 +514,12 @@ exports.default = {
   component: (0, _reactRedux.connect)(mapStateToProps, { fetchUsers: _actions.fetchUsers })(UsersListPage),
   loadData: loadData
 };
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
